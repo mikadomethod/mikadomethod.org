7933e9ef5711407e86b675632144d93804ba6e01
Daniel Brolund <daniel.brolund@agical.com>
2014-11-30 14:51:24 +0100

    All working well

--- tool.js
+++ tool.js
@@ -3,9 +3,10 @@ $("#ss-form").submit(
         e.preventDefault();
         $.ajax({
             url: 'https://docs.google.com/forms/d/1xkm5zs-eE0Uyz6kmXGChBxdcjdNlB2ib-S1CdEbA48o/formResponse',
-            method: 'POST',
+            type: 'POST',
             dataType: 'jsonp',
-            data: {'entry.2024811949' : $('#entry_2024811949').val()},
+            data: {'entry.2024811949' : $('#entry_2024811949').val(),
+                   'entry.769681419': $('#entry_769681419').val()},
             jsonp: "mycallback"
         });
         $('#promo-and-form').hide();
